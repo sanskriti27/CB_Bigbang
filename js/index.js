fbfa5118ae240a0b25a2c31a92644681aef7ed26
Sanskriti <sanskriti.agarwal.sa@gmail.com>
2022-04-26 17:16:15 +0530

    update

--- js/index.js
+++ js/index.js
@@ -23,10 +23,9 @@ submitBtn.addEventListener("click", function (e) {
   // change position or placement of ticketNumber here
   ctx.fillText(enrollInput.value, 325, 805);
   document.getElementById("canvas").style.display = "block";
+  document.getElementById("download").style.display = "block";
   document.getElementById("head").style.display = "block";
   document.getElementById("hide").style.display = "none";
-
-
 });
 
 var name = "";
@@ -43,3 +42,30 @@ background.onload = function () {
   // change position or placement of ticketNumber here
   ctx.fillText(ticketNumber, 325, 805);
 };
+
+const download = document.getElementById("download");
+
+download.addEventListener("click",function(){
+  let url = canvas.toDataURL("image/png");
+  let a = document.createElement("a");
+  a.setAttribute("href",url);
+  a.setAttribute("download", "canvas.png");
+  a.click();
+  a.remove();
+})
+
+// function download(canvas, filename) {
+//   /// create an "off-screen" anchor tag
+//   var lnk = document.createElement("a");
+
+//   /// the key here is to set the download attribute of the a tag
+//   lnk.download = filename;
+
+//   /// convert canvas content to data-uri for link. When download
+//   /// attribute is set the content pointed to by link will be
+//   /// pushed as "download" in HTML5 capable browsers
+//   lnk.href = canvas.toDataURL("image/png,base64");
+
+//   /// create a "fake" click-event to trigger the download
+//   lnk.onclick();
+// }
